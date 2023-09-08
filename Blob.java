4f96e49b88ec6ddefe8778dbc6943403f73fd037
Asher Engelberg <ashercengelberg@icloud.com>
2023-09-07 22:03:48 -0700

    Added some Stuff to Blob
    Sha-1 generate and reads in from a file.

---
+++ Blob.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Blob {
+    public Blob(String string) {
+    }
+
+    public static String doSha(String input) {
+        try {
+            // getInstance() method is called with algorithm SHA-1
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+            // digest() method is called
+            // to calculate message digest of the input string
+            // returned as array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+
+            // Add preceding 0s to make it 32 bit
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            // return the HashText
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String readText(String fileName) throws IOException {
+        String output = "";
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        while (reader.ready()) {
+            int letter = reader.read();
+            output += (char) letter;
+        }
+        reader.close();
+        return output;
+    }
+
+    public static String getShaString(String sha) {
+        return sha;
+    }
+
+}
